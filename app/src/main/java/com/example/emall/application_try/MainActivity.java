da0295fa2935ea1f0cbf5eeb0866366bbe97eb1f
Richa <krutin47trivedi@gmail.com>
2017-06-01 02:52:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/emall/application_try/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.emall.application_try;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private ImageView mImage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mImage.setImageResource(R.drawable.a1);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mImage.setImageResource(R.drawable.a3);
+                    return true;
+                case R.id.navigation_profile:
+                    mImage.setImageResource(R.drawable.a4);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImage = (ImageView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+
+}
